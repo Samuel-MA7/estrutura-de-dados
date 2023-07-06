3aae85217136f3f1cc99f43732c15f54a9787773
Samuel-MA7 <s4muelm0ura@gmail.com>
2023-07-06 09:20:53 -0300

    Pointers of pointers

---
+++ ponteiros/tdc-ponteiros-00.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    float z = 2.5;
+    float *fp;
+
+    fp = &z;
+
+    printf("*&z = %f\n", *&z);
+    printf("*fp = %f\n", *fp);
+    printf("**&fp = %f\n", **&fp);
+
+    return 0;
+
+}
